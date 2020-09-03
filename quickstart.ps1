982ebaf9a351cc45c2781cf694ad0dfa7464d980
Sten Schmidt <sten.schmidt@outlook.com>
2020-09-03 15:17:36 +0200

    Caption added to Powershell-Windows for Powershell Quickstart Demo.

--- quickstart.ps1
+++ quickstart.ps1
@@ -2,15 +2,27 @@
 if (![System.IO.File]::Exists($jarFile)) {
     Write-Error -Message "The file $jarFile does not exist. Please build project first." 
 } else {
-    Write-Host "Starting Server A in a new powershell window. Exit with [Strg]+[C]"
-    start powershell { java -cp .\JTeeProxy.jar test.tools.EchoServer 6789 }
+    start powershell { 
+        $host.ui.RawUI.WindowTitle = “Server A”
+        Write-Host "Server A started. Exit with [Strg]+[C]"
+        java -cp .\JTeeProxy.jar test.tools.EchoServer 6789 
+    }
 
-    Write-Host "Starting Server B in a new powershell window. Exit with [Strg]+[C]"
-    start powershell { java -cp .\JTeeProxy.jar test.tools.EchoServer 6790 }
+    start powershell { 
+        $host.ui.RawUI.WindowTitle = “Server B”
+        Write-Host "Server B started. Exit with [Strg]+[C]"
+        java -cp .\JTeeProxy.jar test.tools.EchoServer 6790 
+    }
 
-    Write-Host "Start the proxy in a new powershell window. Exit with [Strg]+[C]"
-    start powershell { java -cp .\JTeeProxy.jar net.stenschmidt.jteeproxy.JTeeProxy 1234 localhost 6789 localhost 6790 }
+    start powershell { 
+        $host.ui.RawUI.WindowTitle = “JTeeProxy”
+        Write-Host "JTeeProxy started. Exit with [Strg]+[C]"
+        java -cp .\JTeeProxy.jar net.stenschmidt.jteeproxy.JTeeProxy 1234 localhost 6789 localhost 6790 
+    }
 
-    Write-Host "Connect the Client to the Proxy. Exit with [Strg]+[C]. Type some text and press [Enter]:"
-    java -cp .\JTeeProxy.jar test.tools.EchoClient localhost 1234
+    start powershell { 
+        $host.ui.RawUI.WindowTitle = “Echo Client”
+        Write-Host "Client connected to JTeeProxy. Exit with [Strg]+[C]. Type some text and press [Enter]:"
+        java -cp .\JTeeProxy.jar test.tools.EchoClient localhost 1234
+    }
 }
